07cb01728fd73fee0841a0a0e3c960f844f4cc95
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-06-29 14:49:49 -0300

    adicionamdo a sessao de trabalhe conosco

--- js/script.js
+++ js/script.js
@@ -92,7 +92,10 @@ $(document).ready(function () {
 	});
 
 	setTimeout(function() {
+		
 		$('#data-area').parallax({imageSrc:'img/cidadeparallax.png'});
+		$('#apply-area').parallax({imageSrc:'img/pattern.png'});
+
 	}, 250);
 
 
